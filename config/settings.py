d10d4adb1733a10e43db6d6dd9a662ceea75209b
ShinY Kim <shinykim215@gmail.com>
2021-03-17 20:38:28 -0400

    u2

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 """ DEBUG = bool(os.environ.get("DEBUG"))
  """
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = [".elasticbeanstalk.com", "localhost", "127.0.0.1"]
 
@@ -88,25 +88,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG:
-    
+""" if DEBUG: # 로컬용
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            "HOST": os.environ.get("RDS_HOST"),
-            "NAME": os.environ.get("RDS_NAME"),
-            "USER": os.environ.get("RDS_USER"),
-            "PASSWORD": os.environ.get("RDS_PASSWORD"),
-            "PORT": "5432",
-        }
+else: """
+# 서버용
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        "HOST": os.environ.get("RDS_HOST"),
+        "NAME": os.environ.get("RDS_NAME"),
+        "USER": os.environ.get("RDS_USER"),
+        "PASSWORD": os.environ.get("RDS_PASSWORD"),
+        "PORT": "5432",
     }
+}
 
 
 # Password validation
